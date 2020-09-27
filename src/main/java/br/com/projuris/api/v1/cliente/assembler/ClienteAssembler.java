24dd3e27949d832914c59f54817b9ac871ba5983
Victtor <victtorfreitas95@gmail.com>
2020-09-27 16:19:33 -0300

    Adiciona retorno de telefones em clientes

--- src/main/java/br/com/projuris/api/v1/cliente/assembler/ClienteAssembler.java
+++ src/main/java/br/com/projuris/api/v1/cliente/assembler/ClienteAssembler.java
@@ -1,10 +1,13 @@
 package br.com.projuris.api.v1.cliente.assembler;
 
 import br.com.projuris.api.v1.cliente.model.response.ClienteCompletoResponse;
+import br.com.projuris.api.v1.cliente.model.response.TelefoneCompletoResponse;
 import br.com.projuris.domain.cliente.Cliente;
 import br.com.projuris.infrastructure.interfaces.AssamblerDefault;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
 
 @Component
 public class ClienteAssembler implements AssamblerDefault<Cliente, ClienteCompletoResponse> {
@@ -17,6 +20,13 @@ public class ClienteAssembler implements AssamblerDefault<Cliente, ClienteComple
 
     @Override
     public ClienteCompletoResponse toModel(Cliente cliente) {
-        return modelMapper.map(cliente, ClienteCompletoResponse.class);
+        var telefones = cliente.getTelefones()
+                .stream()
+                .map(t -> modelMapper.map(t, TelefoneCompletoResponse.class))
+                .collect(Collectors.toList());
+
+        var clienteCompletoResponse = modelMapper.map(cliente, ClienteCompletoResponse.class);
+        clienteCompletoResponse.setTelefones(telefones);
+        return clienteCompletoResponse;
     }
 }
