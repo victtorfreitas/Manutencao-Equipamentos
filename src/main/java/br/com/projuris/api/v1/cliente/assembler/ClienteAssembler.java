aa8df2f61e634bb062b74e441bb35058e353e817
Victtor <victtorfreitas95@gmail.com>
2020-09-28 09:36:51 -0300

    Atualiza Cliente Assembler

--- src/main/java/br/com/projuris/api/v1/cliente/assembler/ClienteAssembler.java
+++ src/main/java/br/com/projuris/api/v1/cliente/assembler/ClienteAssembler.java
@@ -1,32 +1,38 @@
 package br.com.projuris.api.v1.cliente.assembler;
 
 import br.com.projuris.api.v1.cliente.model.response.ClienteCompletoResponse;
-import br.com.projuris.api.v1.cliente.model.response.TelefoneCompletoResponse;
 import br.com.projuris.domain.cliente.Cliente;
 import br.com.projuris.infrastructure.interfaces.AssamblerDefault;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class ClienteAssembler implements AssamblerDefault<Cliente, ClienteCompletoResponse> {
 
     private final ModelMapper modelMapper;
+    private final TelefoneAssembler telefoneAssembler;
 
-    public ClienteAssembler(ModelMapper modelMapper) {
+    public ClienteAssembler(ModelMapper modelMapper, TelefoneAssembler telefoneAssembler) {
         this.modelMapper = modelMapper;
+        this.telefoneAssembler = telefoneAssembler;
     }
 
     @Override
     public ClienteCompletoResponse toModel(Cliente cliente) {
-        var telefones = cliente.getTelefones()
-                .stream()
-                .map(t -> modelMapper.map(t, TelefoneCompletoResponse.class))
-                .collect(Collectors.toList());
-
         var clienteCompletoResponse = modelMapper.map(cliente, ClienteCompletoResponse.class);
-        clienteCompletoResponse.setTelefones(telefones);
+        if (cliente.temTelefones()) {
+            var telefones = telefoneAssembler.toListModel(cliente.getTelefones());
+            clienteCompletoResponse.setTelefones(telefones);
+        }
+
         return clienteCompletoResponse;
+    }
+
+    @Override
+    public List<ClienteCompletoResponse> toListModel(List<Cliente> clientes) {
+        return clientes.stream().map(this::toModel).collect(Collectors.toList());
     }
 }
