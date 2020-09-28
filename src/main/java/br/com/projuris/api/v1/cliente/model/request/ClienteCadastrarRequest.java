15af86c14a7a91671fbb21230dbb1485ba09c433
Victtor <victtorfreitas95@gmail.com>
2020-09-28 09:43:47 -0300

    Atualiza DTO Cadastrar Cliente

--- src/main/java/br/com/projuris/api/v1/cliente/model/request/ClienteCadastrarRequest.java
+++ src/main/java/br/com/projuris/api/v1/cliente/model/request/ClienteCadastrarRequest.java
@@ -1,13 +1,41 @@
 package br.com.projuris.api.v1.cliente.model.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
 
 @Getter
 @Setter
 public class ClienteCadastrarRequest {
 
+    @NotBlank
+    @Size(min = 5, max = 250)
+    @ApiModelProperty(example = "Victtor Araújo Freitas", required = true)
     private String nome;
+
+    @NotBlank
+    @Size(min = 11, max = 11)
+    @ApiModelProperty(example = "02729712155", required = true)
     private String cpf;
+
+    @Email
+    @ApiModelProperty(example = "victtorfreitas95@gmail.com")
+    private String email;
+
+    @Valid
+    @NotNull
+    private List<TelefoneCadastrarRequest> telefones;
+
+
+    public boolean temTelefones() {
+        return this.telefones != null;
+    }
 
 }
