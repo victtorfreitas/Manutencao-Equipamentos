ac7e2c9f2bd69488bf224edce61896a4c5caa1bd
Victtor <victtorfreitas95@gmail.com>
2020-09-29 09:31:49 -0300

    Adiciona Ordem Servico repository

---
+++ src/main/java/br/com/projuris/domain/ordemservico/repository/listar/OrdemServicoListaRepository.java
@@ -0,0 +1,9 @@
+package br.com.projuris.domain.ordemservico.repository.listar;
+
+import br.com.projuris.domain.ordemservico.OrdemServico;
+import br.com.projuris.infrastructure.interfaces.RepositoryDefault;
+
+public interface OrdemServicoListaRepository extends RepositoryDefault<OrdemServico>, OrdemServicoListaRepositoryCustom {
+
+}
+
---
+++ src/main/java/br/com/projuris/domain/ordemservico/repository/listar/OrdemServicoListaRepositoryCustom.java
@@ -0,0 +1,5 @@
+package br.com.projuris.domain.ordemservico.repository.listar;
+
+public interface OrdemServicoListaRepositoryCustom {
+
+}
