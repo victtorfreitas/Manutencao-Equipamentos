6beb7f9aca8d014151f5854bdc71938b37ac3e7c
Victtor <victtorfreitas95@gmail.com>
2020-09-29 09:23:13 -0300

    Adiciona Funcionario model

--- src/main/java/br/com/projuris/domain/ordemservico/OrdemServico.java
+++ src/main/java/br/com/projuris/domain/ordemservico/OrdemServico.java
@@ -2,24 +2,32 @@ package br.com.projuris.domain.ordemservico;
 
 import br.com.projuris.domain.cliente.Cliente;
 import br.com.projuris.domain.equipamento.Equipamento;
+import br.com.projuris.domain.funcionario.Funcionario;
 import br.com.projuris.infrastructure.abstracts.EntityAuditAbsDefault;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Entity(name = "TB_ORDEM_SERVICO")
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
+@Where(clause = "IS_ATIVO=true")
 public class OrdemServico extends EntityAuditAbsDefault {
 
     @NotNull
@@ -31,6 +39,7 @@ public class OrdemServico extends EntityAuditAbsDefault {
     private Cliente cliente;
 
     @NotNull
+    @CreationTimestamp
     private LocalDateTime dataInicio;
 
     private LocalDateTime dataFim;
@@ -38,4 +47,10 @@ public class OrdemServico extends EntityAuditAbsDefault {
     @NotNull
     @Enumerated(EnumType.STRING)
     private StatusOrdemServicoEnum status;
+
+    @ManyToMany
+    @JoinTable(name = "TB_FUNCIONARIOS_OS",
+            joinColumns = @JoinColumn(name = "ID_ORDEM_SERVICO"),
+            inverseJoinColumns = @JoinColumn(name = "ID_FUNCIONARIO"))
+    private List<Funcionario> funcionarios;
 }
