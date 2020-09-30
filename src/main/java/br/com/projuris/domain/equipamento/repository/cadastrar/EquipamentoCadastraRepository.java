be067b6eed6356a80b919055899391d0e05770d1
Victtor <victtorfreitas95@gmail.com>
2020-09-30 11:38:53 -0300

    Adiciona equipamento Repository

---
+++ src/main/java/br/com/projuris/domain/equipamento/repository/cadastrar/EquipamentoCadastraRepository.java
@@ -0,0 +1,9 @@
+package br.com.projuris.domain.equipamento.repository.cadastrar;
+
+import br.com.projuris.domain.equipamento.Equipamento;
+import br.com.projuris.infrastructure.interfaces.RepositoryDefault;
+
+public interface EquipamentoCadastraRepository extends RepositoryDefault<Equipamento>, EquipamentoCadastraRepositoryCustom {
+
+}
+
---
+++ src/main/java/br/com/projuris/domain/equipamento/repository/cadastrar/EquipamentoCadastraRepositoryCustom.java
@@ -0,0 +1,5 @@
+package br.com.projuris.domain.equipamento.repository.cadastrar;
+
+public interface EquipamentoCadastraRepositoryCustom {
+
+}
